c974a00f2efa0cdeada1b375d1acb104180c5fdf
Gary84M <86193312+Gary84M@users.noreply.github.com>
2021-10-25 22:58:34 +0200

    cart done

--- front/js/model/Cart.js
+++ front/js/model/Cart.js
@@ -1,16 +1,16 @@
 export class Cart {
-  constructor(id, color, qty) {
-    this.id = id;
-    this.color = color;
-    this.qty = qty;
-
+  constructor() {
     this.items = [];
-
-    //this.load();
+    this._load();
   }
 
-  addProduct(id, qty, color) {
+  addProduct(product, qty, color) {
+    //TODO veirfier tes types de données qyt number > 0 ...
+
+    //TODO verifier avant insertion s'il ne faudrait pas plutot incrementer un item existant
+
     //ajouter produit à this.items
+    this.items.push({ product, qty, color });
 
     this._save();
   }
@@ -19,11 +19,23 @@ export class Cart {
     this._save();
   }
 
-  _save() {
-    //stocker this.items dans ton localSotrage
+  getTotal() {
+    // TODO calculer le total du panier
+    return 0;
   }
 
-  load() {
+  clear() {
+    this.items = [];
+    this._save();
+  }
+
+  _save() {
+    //stocker this.items dans ton localSotrage
+    localStorage.setItem("cartItems", JSON.stringify(this.items));
+  }
+
+  _load() {
     //recuperer depuis storage this.items
+    this.items = JSON.parse(localStorage.getItem("cartItems")) || [];
   }
 }
--- front/js/product.js
+++ front/js/product.js
@@ -1,65 +1,58 @@
 import { API_PRODUCTS_LIST_LINK } from "./constant.js";
 import { Cart } from "./model/Cart.js";
 
-const addToCartButton = document.querySelectorAll(".item__content__addButton");
-for (let i = 0; i < addToCartButton.length; i++) {
-  addToCartButton[i].addEventListener("click", () => {
-    //console.log("num of i");
-    entriesToCart();
-  });
-}
-
-let entriesToCart = () => {
-  let productNumbers = localStorage.getItem("entriesToCart");
-  console.log(productNumbers);
-  console.log(typeof productNumbers);
-
-  productNumbers = parseInt(productNumbers);
-  console.log(typeof productNumbers);
-
-  localStorage.setItem("entriesToCart", 1 + productNumbers);
-};
-
 const cart = new Cart();
-//lors du clic sur Ajouter au panier, il faudrait appeler la methode cart.addProduct(id,qty,color)
 
-const queryStringURLId = window.location.search;
+const addToCartButton = document.getElementById("addToCart");
+const couchQuantityInput = document.getElementById("quantity");
+const couchColorsSelector = document.getElementById("colors");
+const couchImgContainer = document.querySelector(".item__img");
+const couchDescriptionContainer = document.querySelector(
+  ".item__content__description"
+);
+const couchTitlePriceContainer = document.querySelector(
+  ".item__content__titlePrice"
+);
+
 // get URL from the "?" mark
-const urlSearchParams = new URLSearchParams(queryStringURLId);
+const queryStringURLId = window.location.search;
 //USP called as a new instance with queryStringURLId argument
-const id = urlSearchParams.get("id");
+const urlSearchParams = new URLSearchParams(queryStringURLId);
 //create const id
+const id = urlSearchParams.get("id");
+
+couchSpecs(id).then((couch) => {
+  colorPicker(couch.colors);
+  specsInjection(couch);
+  addToCartButton.addEventListener("click", () => {
+    cart.addProduct(couch, couchQuantityInput.value, couchColorsSelector.value);
+  });
+});
 
 //Iteration of color of data.colors w/ arg data + create markup <option>
-function colorPicker(data) {
-  for (let color of data.colors) {
+function colorPicker(colors) {
+  for (let color of colors) {
     let option = document.createElement("option");
     option.value = color;
     option.text = color;
-    document.getElementById("colors").appendChild(option);
+    couchColorsSelector.appendChild(option);
   }
 }
 //Call the API + id. declare: data. call API, specsInjection & colorPicker
 async function couchSpecs(id) {
   const response = await fetch(API_PRODUCTS_LIST_LINK + "/" + id);
-  const data = await response.json();
-  const { colors, _id, name, price, imageURL, description, altTxt } = data;
-  console.log(data.colors);
-  specsInjection(data);
-
-  colorPicker(data);
+  return await response.json();
 }
-couchSpecs(id);
 
 const specsInjection = (data) => {
-  document.querySelector(".item__img").innerHTML = `
+  couchImgContainer.innerHTML = `
       <img src="${data.imageUrl}" alt="${data.altTxt}">
   `;
-  document.querySelector(".item__content__titlePrice").innerHTML = `
+  couchTitlePriceContainer.innerHTML = `
         <h1 id="title"> ${data.name}</h1>
         <p>Prix : <span id="price">${data.price}</span>€</p>
   `;
-  document.querySelector(".item__content__description").innerHTML = `
+  couchDescriptionContainer.innerHTML = `
     <p class="item__content__description__title">Description :</p>
     <p id="description">${data.description}</p>
   
